4c86a508e92e568ee46b95c1c2d87a6af8733cd9
Chanson9892 <chan.hanson5@gmail.com>
2021-01-04 18:28:44 -0800

    fixed valid palindrome

--- DSA_Questions/twoSum.js
+++ DSA_Questions/twoSum.js
@@ -1,6 +1,6 @@
 // Given a 32-bit signed integer, reverse digits of an integer.
 // Assume we are dealing with an environment that could only store integers within the 32-bit signed integer 
-// range: [−231, 231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+// range: [−2^31, 2^31 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 // Example 1:
 // Input: x = 123
 // Output: 321
